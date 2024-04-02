ba75b7fa9f47dbdccd6208f6b0ddd2c608444624
James Chen <chenhao540@gmail.com>
2024-04-02 09:03:32 -0700

    rm unused styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <footer className="bg-transparent shadow">
         <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 By Hao Chen. All Rights Reserved.</span>
-          <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+          <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500">
               <li>
                   <a href="/" className="hover:underline me-4 md:me-6">About</a>
               </li>
